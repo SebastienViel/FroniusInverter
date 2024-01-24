52ecb509a93a0ef2d86a026a7e7968282258cb49
Sébastien <58109049+SebastienViel@users.noreply.github.com>
2024-01-23 21:36:02 -0400

    Update FroniusSolarInverter.groovy
    Cleaned-up and added consistency to the code.

--- FroniusSolarInverter.groovy
+++ FroniusSolarInverter.groovy
@@ -60,137 +60,127 @@ metadata {
 
 def initialize() {
 	if(logDebugEnable) log.info "Fronius Inverter ${textVersion()} ${textCopyright()}"
-    sendEvent(name: "power", value: 0	)
-    sendEvent(name: "YearValue", value: 0 )
-    sendEvent(name: "energy", value: 0 )
-    sendEvent(name: "pGrid", value: 0 )
-    sendEvent(name: "pLoad", value: 0 )
-    sendEvent(name: "TotalValue", value: 0 )
-    state.parseCallCounter=0
-//    updated()
+	sendEvent(name: "power", value: 0	)
+	sendEvent(name: "YearValue", value: 0 )
+	sendEvent(name: "energy", value: 0 )
+	sendEvent(name: "pGrid", value: 0 )
+	sendEvent(name: "pLoad", value: 0 )
+	sendEvent(name: "TotalValue", value: 0 )
+	state.parseCallCounter=0
+//    	  updated()
 	poll()
 }
 
-
 // parse events into attributes
 def parse(String description) {	
-    def msg = parseLanMessage(description)
-
+	def msg = parseLanMessage(description)
 	def slurper = new JsonSlurper()
-    def result = slurper.parseText(msg.body)
-    def errorCode = result?.Body?.Data?."$inverterNumber"?.ErrorCode
-    if(errorCode != null) {
-        sendEvent(name: "errorCode", value: errorCode )
-    } else {
-        sendEvent(name: "errorCode", value: 0 )
-	if(logDebugEnable) log.debug "Received data from inverter: ${result.Body.Data.Site}"
+	def result = slurper.parseText(msg.body)
+	def errorCode = result?.Body?.Data?."$inverterNumber"?.ErrorCode
+	if(errorCode != null) {
+        	sendEvent(name: "errorCode", value: errorCode )
+	} else {
+		sendEvent(name: "errorCode", value: 0 )
+		if(logDebugEnable) log.debug "Received data from inverter: ${result.Body.Data.Site}"
 
-        int yearValue = result.Body.Data.Site.E_Year
-        int dayValue = result.Body.Data.Site.E_Day
-        int totalValue = result.Body.Data.Site.E_Total
-	def pGridValue = result.Body.Data.Site.P_Grid
-	int pGrid = pGridValue != null ? pGridValue.toInteger() : 0
-        def pLoadValue = result.Body.Data.Site.P_Load
-        int pLoad = pLoadValue != null ? pLoadValue.toInteger() : 0
-	    pLoad = -pLoad
-        def pPV = result.Body.Data.Site.P_PV
-        int power = pPV != null ? pPV.toInteger() : 0
-//        if (pPV == null) {
-//            power = 0
-//        } else {
-//            power = pPV
-//        }
-        sendEvent(name: "power", value: power, unit: "W" )
-        sendEvent(name: "energy", value: dayValue, unit: "Wh")
-        sendEvent(name: "eYear", value: yearValue / 1000, unit: "kWH")
-        def value = new BigDecimal(totalValue / 1000).setScale(3, BigDecimal.ROUND_HALF_UP)
-	sendEvent(name: "TotalEnergy", value: value, unit: "kWH")
+        	int yearValue = result.Body.Data.Site.E_Year
+		int dayValue = result.Body.Data.Site.E_Day
+        	int totalValue = result.Body.Data.Site.E_Total
+		def pGridValue = result.Body.Data.Site.P_Grid
+		int pGrid = pGridValue != null ? pGridValue.toInteger() : 0
+        	def pLoadValue = result.Body.Data.Site.P_Load
+        	int pLoad = pLoadValue != null ? pLoadValue.toInteger() : 0
+			pLoad = -pLoad
+        	def pPV = result.Body.Data.Site.P_PV
+        	int power = pPV != null ? pPV.toInteger() : 0
 
-	    
-	sendEvent(name: "pGrid", value: pGrid, unit: "W")
-        sendEvent(name: "pLoad", value: pLoad, unit: "W")
-        //Keep track of when the last update came in
-        if (state.parseCallCounter>0)
-        {
-            updated()    // reset the poll timer back to 1 min if we got an answer
-            sendEvent(name: "Status", value: "Online")
-        }
-        state.parseCallCounter=0
+		sendEvent(name: "power", value: power, unit: "W" )
+        	sendEvent(name: "energy", value: dayValue, unit: "Wh")
+        	sendEvent(name: "eYear", value: yearValue / 1000, unit: "kWH")
+        	def value = new BigDecimal(totalValue / 1000).setScale(3, BigDecimal.ROUND_HALF_UP)
+		sendEvent(name: "TotalEnergy", value: value, unit: "kWH")
+		sendEvent(name: "pGrid", value: pGrid, unit: "W")
+        	sendEvent(name: "pLoad", value: pLoad, unit: "W")
 
-	// Log the received data for debugging
-        if(logDebugEnable) log.debug "Received data from inverter: ${result}"
-    }
+		//Keep track of when the last update came in
+        	if (state.parseCallCounter>0) {
+        		updated()    // reset the poll timer back to 1 min if we got an answer
+            		sendEvent(name: "Status", value: "Online")
+        	}
+        	state.parseCallCounter=0
+
+		// Log the received data for debugging
+        	if(logDebugEnable) log.debug "Received data from inverter: ${result}"
+    	}
 }
 
 // handle commands
 def poll() {
-    if (state.parseCallCounter==null) {
-        state.parseCallCounter=1
-    } else {
-        state.parseCallCounter++
-    }
-    if (state.parseCallCounter==3)
-    {
-        // Not getting Fronius's replies so could be sleeping.  Set the timer with 30 min interval
-        sendEvent(name: "Status", value: "Not Responding")
-        unschedule(refresh)
-        schedule('0 */30 * ? * *', refresh)
-    }
-    callInvertor()
+	if (state.parseCallCounter==null) {
+        	state.parseCallCounter=1
+    	} else {
+        	state.parseCallCounter++
+    	}
+    	if (state.parseCallCounter==3) {
+        	// Not getting Fronius's replies so could be sleeping.  Set the timer with 30 min interval
+        	sendEvent(name: "Status", value: "Not Responding")
+        	unschedule(refresh)
+        	schedule('0 */30 * ? * *', refresh)
+    	}
+    	callInvertor()
 }
 
 
 def callInvertor() {
-	try
-    {
-	def hosthex = convertIPtoHex(destIp)
-    def porthex = convertPortToHex(destPort)
-    device.deviceNetworkId = "$hosthex:$porthex" 
+	try {
+		def hosthex = convertIPtoHex(destIp)
+    		def porthex = convertPortToHex(destPort)
+    		device.deviceNetworkId = "$hosthex:$porthex" 
 
-    sendHubCommand(new hubitat.device.HubAction(
+    		sendHubCommand(new hubitat.device.HubAction(
    	 		'method': 'GET',
-    		'path': "/solar_api/v1/GetInverterInfo.cgi?Scope=System",
-        	'headers': [ HOST: "$destIp:$destPort" ]
-		))
+    			'path': "/solar_api/v1/GetInverterInfo.cgi?Scope=System",
+        		'headers': [ HOST: "$destIp:$destPort" ]
+			))
 
-//This is the call to get data	    
-    def hubAction = new hubitat.device.HubAction(
+		//This is the call to get data	    
+   		def hubAction = new hubitat.device.HubAction(
    	 		'method': 'GET',
-    		'path': "/solar_api/v1/GetPowerFlowRealtimeData.fcgi",
-        	'headers': [ HOST: "$destIp:$destPort" ]
-		) 
-    hubAction
+    			'path': "/solar_api/v1/GetPowerFlowRealtimeData.fcgi",
+        		'headers': [ HOST: "$destIp:$destPort" ]
+			) 
+    		hubAction
 	    
-    }
-    catch (Exception e) {
+    	}
+    	catch (Exception e) {
         if(logDebugEnable) log.debug "Hit Exception $e on $hubAction"
-    }
+    	}
 }
 
 private def textVersion() {
-    def text = "Version 1.0"
+    	def text = "Version 1.1"
 }
 
 private def textCopyright() {
-    def text = "Copyright © 2015 Tim Flinders"
+    	def text = "Copyright © 2015 Tim Flinders"
 }
 
 private String convertIPtoHex(ipAddress) { 
-    String hex = ipAddress.tokenize( '.' ).collect {  String.format( '%02X', it.toInteger() ) }.join()
-    return hex
+    	String hex = ipAddress.tokenize( '.' ).collect {  String.format( '%02X', it.toInteger() ) }.join()
+    	return hex
 }
 
 private String convertPortToHex(port) {
 	String hexport = port.toString().format( '%04X', port.toInteger() )
-    return hexport
+    	return hexport
 }
 
 def refresh(){
-    poll()
+	poll()
 }
 
 def updated(){
-    unschedule(refresh)
-    schedule('0 */1 * ? * *', refresh)
-    state.parseCallCounter=0
+    	unschedule(refresh)
+    	schedule('0 */1 * ? * *', refresh)
+    	state.parseCallCounter=0
 }
